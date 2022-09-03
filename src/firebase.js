2b2278c4a01564f58a6379922b23570faa1354d0
jodelajo <jodelajo@gmail.com>
2022-09-03 12:34:36 +0200

    add google analytics

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 // import "firebase/auth"
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -9,11 +10,13 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
   
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app)
 
 export const auth = getAuth(app)
 export default app
